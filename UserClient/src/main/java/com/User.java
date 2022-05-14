68aab37fc2210cd0e4b08e63452414ae97bcda5c
Isuru Prabhath <it20012342@my.sliit.lk>
2022-05-14 16:58:04 +0530

    Login, Logout & Retrieve User Parts Done

--- UserClient/src/main/java/com/User.java
+++ UserClient/src/main/java/com/User.java
@@ -1,5 +1,76 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class User {
+	public Connection connect() {
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid", "root", "root");
+			// For testing
+			System.out.print("Successfully connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+	
+	public String readUsers() 
+	 { 
+	 String output = ""; 
+	 try
+	 { 
+	 Connection con = connect(); 
+	 if (con == null) 
+	 { 
+	 return "Error while connecting to the database for reading."; 
+	 } 
+	 // Prepare the html table to be displayed
+	 output = "<table border='1'><tr><th>User ID</th> <th>User Name</th><th>Account Number</th>"
+	 + "<th>Address</th> <th>NIC</th> <th>Phone</th> <th>User ROle</th><th>Update</th><th>Remove</th></tr>"; 
+	 String query = "select * from user"; 
+	 Statement stmt = con.createStatement(); 
+	 ResultSet rs = stmt.executeQuery(query); 
+	 // iterate through the rows in the result set
+	 while (rs.next()) 
+	 { 
+		 String userID = Integer.toString(rs.getInt("userID"));
+			String username = rs.getString("username");
+			String address = rs.getString("address");
+			String accountNumber = Integer.toString(rs.getInt("accountNumber"));
+			String NIC = rs.getString("NIC");
+			String phone = rs.getString("phone");
+			String role = rs.getString("userRole");
+	 // Add into the html table
+	output += "<tr><td><input id='hidItemIDUpdate' name='hidItemIDUpdate' type='hidden' value='" + userID
+	 + "'>" + "</td>"; 
+	output += "<tr><td>" + userID + "</td>";
+	output += "<td>" + username + "</td>";
+	output += "<td>" + accountNumber + "</td>";
+	output += "<td>" + address + "</td>";
+	output += "<td>" + NIC + "</td>";
+	output += "<td>" + phone + "</td>";
+	output += "<td>" + role + "</td>";
+	 // buttons
+	output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary' data-itemid='"+ userID + "'>" + "</td>"
+	 + "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-itemid='"+ userID + "'>" + "</td></tr>"; 
+	 } 
+	 con.close(); 
+	 // Complete the html table
+	 output += "</table>"; 
+	 } 
+	 catch (Exception e) 
+	 { 
+	 output = "Error while reading the items."; 
+	 System.err.println(e.getMessage()); 
+	 } 
+	 return output; 
+	 } 
 
 }
--- UserClient/src/main/webapp/users.jsp
+++ UserClient/src/main/webapp/users.jsp
@@ -1,12 +1,53 @@
+<%@page import="com.User"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
+<%
+if (session.getAttribute("Username") == null) {
+	response.sendRedirect("index.jsp");
+}
+%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Users Management</title>
+<link rel="stylesheet" href="Views/bootstrap.min.css">
+<script src="Components/jquery.min.js"></script>
+<script src="Components/users.js"></script>
+<script src="Components/auth.js"></script>
 </head>
 <body>
-
+	<div>
+		<button id="btnLogout">LogOut</button>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-6">
+				<h1>Users Management V10.1</h1>
+				<form id="formItem" name="formItem">
+					Item code: <input id="itemCode" name="itemCode" type="text"
+						class="form-control form-control-sm"> <br> Item name:
+					<input id="itemName" name="itemName" type="text"
+						class="form-control form-control-sm"> <br> Item
+					price: <input id="itemPrice" name="itemPrice" type="text"
+						class="form-control form-control-sm"> <br> Item
+					description: <input id="itemDesc" name="itemDesc" type="text"
+						class="form-control form-control-sm"> <br> <input
+						id="btnSave" name="btnSave" type="button" value="Save"
+						class="btn btn-primary"> <input type="hidden"
+						id="hidItemIDSave" name="hidItemIDSave" value="">
+				</form>
+				<div id="alertSuccess" class="alert alert-success"></div>
+				<div id="alertError" class="alert alert-danger"></div>
+				<br>
+				<div id="divItemsGrid">
+					<%
+					User userObj = new User();
+					out.print(userObj.readUsers());
+					%>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
